36fe6d782489e8e1084ac554d98f18185133f4f4
Antonio Javier <vysite.margen@gmail.com>
2026-03-30 18:54:48 +0200

    Fix apikey header

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,6 +3,13 @@ import { createBrowserClient } from '@supabase/ssr'
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      global: {
+        headers: {
+          apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        },
+      },
+    }
   )
 }
